61ff7c3fe001c6a672f1de074b288b59209bbb4e
mx-personal <maximefer@hotmail.fr>
2021-12-05 22:00:56 +0000

    Adding necessary git files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='osiris',
+  packages=['osiris'],   # Chose the same as "name"
+  version='0.5',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description='Emulator for real life rhythm',   # Give a short description about your library
+  author='mx-personal',                   # Type in your name
+  url='https://github.com/mx-personal/osiris',   # Provide either the link to your github or to your website
+  keywords=['osiris'],   # Keywords that define your package best
+  include_package_data=True,
+  package_data={'': ['*.txt', '*.csv']},
+)
