10d50f349ee9d71e17d8fad42ba56a2a4a9b4e11
mx-personal <maximefer@hotmail.fr>
2021-12-05 22:13:34 +0000

    corrections

--- osiris/main.py
+++ osiris/main.py
@@ -1,11 +1,16 @@
 from .agent import Agent
 from .context import Clock
 
-if __name__ == "__main__":
+
+def main():
     clock = Clock()
     koro = Agent("koro",sim_step=clock.time_step)
     for i in range(24*10*10):
         koro.pick_action(clock.time)
         clock.tick()
     koro.display_results()
-    import pdb;pdb.set_trace()
+    import pdb; pdb.set_trace()
+
+
+if __name__ == "__main__":
+    main()
