6af31afad8799241e7a4931428eda9b183e11464
mx-personal <maximefer@hotmail.fr>
2022-02-19 21:09:50 +0000

    Fixing setup

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
   url='https://github.com/mx-personal/osiris',   # Provide either the link to your github or to your website
   keywords=['osiris'],   # Keywords that define your package best
   include_package_data=True,
-  packages=find_packages(),   # Chose the same as "name"
-  package_data={'*': ['*.txt', '*.csv']},
+  packages=find_packages("osiris"),   # Chose the same as "name"
+  package_data={'': ['*.txt', '*.csv']},
 )
